8681392e510cf56d2be19b37f5ab665cf927a358
BoudewijnF74 <55194947+BoudewijnF74@users.noreply.github.com>
2020-01-15 12:03:26 +0100

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,21 @@
+
+
+function setup() {
+    
+    createCanvas(600, 800);
+
+}
+
+function draw() {
+
+    background(100, 100, 255);    
+
+    fill(255, 255, 0);
+    triangle(100, 200, 300, 0, 500, 200);
+    
+    fill(255, 0, 0);  
+    rect(100, 200, 400, 600);
+
+    fill(255, 255, 0);
+    rect(0, 0, 100, 200)  
+}   
